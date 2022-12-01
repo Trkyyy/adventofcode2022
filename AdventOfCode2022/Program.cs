c7bdc725f395f5cf75bf4b8020700305d5e5e5ce
Turkey <spearzyy@gmail.com>
2022-12-01 11:23:02 +0000

    Day 1

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace AdventOfCode2022
 {
@@ -6,7 +8,23 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string calInput = System.IO.File.ReadAllText("K:/repos/AdventOfCode2022/AdventOfCode2022/Day1/inputD.txt");
+            string[] elfCal = calInput.Split(new string[] { "\r\n\r\n" },
+                               StringSplitOptions.RemoveEmptyEntries);
+            List<int> vals = new List<int>();
+            foreach (string strings in elfCal)
+            {
+                string[] eachCalValue = strings.Split("\r\n");
+                int totalVal = 0;
+                foreach(string calValue in eachCalValue)
+                {
+                    totalVal += Convert.ToInt32(calValue);
+                }
+                vals.Add(totalVal);
+            }
+            vals.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine(Convert.ToString(vals[0]));
+            Console.WriteLine(Convert.ToString(vals[0] + vals[1] + vals[2]));
         }
     }
 }
